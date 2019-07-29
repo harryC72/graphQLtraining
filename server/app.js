a01bbf19c0519c019d98adc0c86777f3a4b69cb7
HarryC72 <haraldcarlsten@yahoo.se>
2019-07-29 11:16:22 +0200

    committing before restarting to get eslint up and running

--- server/app.js
+++ server/app.js
@@ -4,10 +4,9 @@ const schema = require('./schema/schema');
 const mongoose = require('mongoose');
 const app = express();
 
-mongoose.connect('mongodb+srv://harryx:retarded44%@graphqltest-rnybh.azure.mongodb.net/test?retryWrites=true&w=majority');
-
+mongoose.connect('mongodb+srv://harryx:rorman66@graphqltest-rnybh.azure.mongodb.net/test?retryWrites=true&w=majority');
 mongoose.connection.once('open', () => {
-    console.log('connected to database');
+  console.log('connected to database');
 })
 
 app.use('/graphql', graphqlHTTP({
