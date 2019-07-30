656e6ad6242cf8f55053d3e64165884a6b3f9278
HarryC72 <haraldcarlsten@yahoo.se>
2019-07-30 23:54:28 +0200

    now, at last, I fixed the eslint problems. I am awesome!

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,12 @@
 import React from 'react';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
-import BookList from './BookList';
-
-const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql'
-})
+import BookList from './components/BookList';
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <div id="main">
-        <h1>Harry&apos;s reading list</h1>
-        <BookList />
-      </div>
-    </ApolloProvider>
+    <div id="main">
+      <h2>Harald&apos;s Book List</h2>
+      <BookList />
+    </div>
   );
 }
 
---
+++ client/src/components/BookList.js
@@ -0,0 +1,15 @@
+import React, { PureComponent } from 'react';
+
+class BookList extends PureComponent {
+  render() {
+    return (
+      <div>
+        <ul id="book-list">
+          <li>Book name</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default BookList;
